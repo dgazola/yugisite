e37e47c0ed1ca710da3d6f7db5bd2ef909513231
dgazola <rzgaz0lla@gmail.com>
2026-05-07 13:40:21 +0000

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,3 @@
-// js/constants.js
 const SNAP_SPEED_FACTOR = 0.028;
 const SNAP_MIN_DURATION = 0.18;
 const SNAP_MAX_DURATION = 0.72;
@@ -7,3 +6,7 @@ const DRAG_THRESHOLD = 4;
 const DEADZONE_X_FACTOR = 0.15;
 const DEADZONE_Y_FACTOR = 0.15;
 const CURRENT_CARD_WEIGHT = 1.5;
+
+// Axis lock
+const AXIS_LOCK_MIN = 8;        // min pixels to lock an axis
+const AXIS_SWITCH_THRESHOLD = 30; // perpendicular distance to force axis switch
