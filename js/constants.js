0756ee5683b0e363ec53cfe52c80c5576f48da94
dgazola <rzgaz0lla@gmail.com>
2026-05-07 14:25:22 +0000

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -3,8 +3,8 @@ const SNAP_MIN_DURATION = 0.18;
 const SNAP_MAX_DURATION = 0.72;
 const SNAP_EASING = 'cubic-bezier(0.22, 0.1, 0.22, 1.0)';
 const DRAG_THRESHOLD = 4;
-const DEADZONE_X_FACTOR = 0.15;
-const DEADZONE_Y_FACTOR = 0.15;
+const DEADZONE_X_FACTOR = 0.05;
+const DEADZONE_Y_FACTOR = 0.05;
 const CURRENT_CARD_WEIGHT = 10;
 
 // Axis lock
