49cc83d580881bf6cb3f547e1ae1cbed0d5c7d14
dgazola <rzgaz0lla@gmail.com>
2026-05-07 17:34:09 -0300

    Create helpers.js

---
+++ editor/js/helpers.js
@@ -0,0 +1,41 @@
+// Utility functions for the editor
+
+function editorGenerateId() {
+  return 'card-' + Date.now().toString(36) + '-' + Math.random().toString(36).substr(2, 4);
+}
+
+function editorEnsureTranslations(obj, langs) {
+  if (!obj) obj = {};
+  langs.forEach(lang => {
+    if (!obj[lang]) obj[lang] = (lang === 'en' ? '' : (obj['en'] || ''));
+  });
+  return obj;
+}
+
+function editorEscapeHtml(str) {
+  const div = document.createElement('div');
+  div.textContent = str;
+  return div.innerHTML;
+}
+
+function editorReorderAll() {
+  ['main','devlog','blog'].forEach(col => {
+    let order = 0;
+    window.editorData.cards.forEach(c => { if (c.column === col) c.order = order++; });
+  });
+}
+
+function editorMigrateOldCard(c) {
+  return {
+    column: c.column || "main",
+    order: c.order || 0,
+    type: c.type || c.column,
+    id: c.id || editorGenerateId(),
+    uiMode: c.uiMode || 'opaque',
+    imageUrl: c.imageUrl || null,
+    videoUrl: c.videoUrl || null,
+    translations: {
+      en: { name: c.name||"", sub: c.sub||"", label: c.label||"", title: c.title||"", description: c.description||"", meta: c.meta||"", tag: c.tag||"" }
+    }
+  };
+}
